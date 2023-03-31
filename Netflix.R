02cced514ccf474a7c5832c05a0756d06edb0a87
Dan Shield <dshield@unomaha.edu>
2023-03-31 00:15:58 -0500

    Moving legend and fixing range

--- Netflix.R
+++ Netflix.R
@@ -2,6 +2,9 @@ library(plotly)
 
 data <- read.csv('./choropleth.csv')
 
+data$Total[data$Total <= 10] <- 10
+data$Total[data$Total >= 1000] <- 1000
+
 # light grey boundaries
 l <- list(color = toRGB("grey"), width = 0.5)
 
@@ -9,6 +12,7 @@ l <- list(color = toRGB("grey"), width = 0.5)
 g <- list(
   showframe = FALSE,
   showcoastlines = FALSE,
+  colorbar = 'bottom',
   projection = list(type = 'mercator')
 )
 
@@ -17,11 +21,15 @@ fig <- fig %>% add_trace(
   z = ~Total, color = ~Total, colors = 'Blues',
   text = ~Country, locations = ~code, marker = list(line = l)
 )
-fig <- fig %>% colorbar(title = 'Year Launched')
+
+fig <- fig %>% colorbar(title = 'Titles', x = 0.5, y=0.33, orientation="h", tickangle="-45", len = ".9",
+                        tickvals = c(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000),
+                        ticktext = c('Less than 100','200', '300', '400', '500', '600', '700', '800', '900','More than 1000'))
 fig <- fig %>% layout(
-  title = 'Number of Titles Availalbe on Netflix',
+  title = 'Number of Titles Available on Netflix by Country',
   geo = g
 )
 
 fig
+
 
